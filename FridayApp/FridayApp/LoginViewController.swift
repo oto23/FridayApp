7dc4b84fc60213fc9a572f661efe51f64756218f
Talakhadze2001 <nikusha13.talakhadze@gmail.com>
2019-02-18 00:38:00 +0400

    nikolita

---
+++ FridayApp/FridayApp/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  FridayApp
+//
+//  Created by Nika Talakhadze on 2/18/19.
+//  Copyright © 2019 None. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SkyFloatingLabelTextField
+
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+    }
+    
+    
+    
+}
