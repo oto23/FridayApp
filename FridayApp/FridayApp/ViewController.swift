86fef63d47554ebb8b65c346e7beb0da7c36b3b8
Oto <oto_machavariani_ai@geolab.edu.ge>
2019-02-17 19:29:24 +0400

    setup

---
+++ FridayApp/FridayApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FridayApp
+//
+//  Created by Macbook pro on 2/17/19.
+//  Copyright © 2019 None. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
